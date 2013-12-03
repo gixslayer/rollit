309380040abeeab46ead47023333da271a9d4d56
gixslayer <ciske_harsema_2@hotmail.com>
2013-12-03 15:34:33 +0100

    Added underlying byte representation

--- Shared/src/org/insomnia/rollit/shared/rollit/Ball.java
+++ Shared/src/org/insomnia/rollit/shared/rollit/Ball.java
@@ -1,9 +1,37 @@
 package org.insomnia.rollit.shared.rollit;
 
 public enum Ball {
-	Blue,
-	Red,
-	Yellow,
-	Green,
-	None;
+	None((byte)0),
+	Blue((byte)1),
+	Red((byte)2),
+	Yellow((byte)3),
+	Green((byte)4);
+	
+	private byte value;
+	
+	Ball(byte value) {
+		this.value = value;
+	}
+	
+	public byte getValue() {
+		return value;
+	}
+	
+	public static Ball fromByte(byte value) {
+		Ball ball = null;
+		
+		if(value == Blue.getValue()) {
+			ball = Blue;
+		} else if(value == Red.getValue()) {
+			ball = Red;
+		} else if(value == Yellow.getValue()) {
+			ball = Yellow;
+		} else if(value == Green.getValue()) {
+			ball = Green;
+		} else if(value == None.getValue()) {
+			ball = None;
+		}
+		
+		return ball;
+	}
 }
